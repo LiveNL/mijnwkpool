04c1b354b76a3f5dc614164a33a8c65560d2aa74
Sander Tuin <github@sandert.me>
2014-03-12 13:50:24 +0100

    Test controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Wkpool::Application.routes.draw do
+
+  root  "test#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
