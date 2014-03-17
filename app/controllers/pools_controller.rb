836247f97922aa9bf1bce6b5d6b3cd9339744905
Jordi Wippert <jor_40bmx@hotmail.com>
2014-03-17 15:27:26 +0100

    Add create function in pools_controller, add current_user to application_controller

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,4 +1,13 @@
 class PoolsController < ApplicationController
-  def index
+  def create
+  	@pool = Pool.new(pool_params)	
+  	if @pool.save
+  		@pool.poolmemberships.create(:user => current_user)
+  	end	
   end
+
+  private
+    def pool_params
+      params.require(:pool).permit(:name)
+    end
 end
