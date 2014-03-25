8a4ee1df1d9b238afc41041f5f417905f5acb97f
Peter Alserda <peteralserda@hotmail.com>
2014-03-25 15:09:24 +0100

    error message fix

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -5,7 +5,6 @@ class Pool < ActiveRecord::Base
 	validates_presence_of :name, message: "* Het veld mag niet leeg zijn!"
 	validates_length_of :name, :in => 3..35, message: "* De pool naam moet tussen 3-35 karakters hebben! "
 
-	
 	validates_confirmation_of :password, if: :private_pool?, message: "* De wachtwoorden komen niet overeen!"
  	validates_length_of :password, :in => 7..15, if: :private_pool?, message: "* Het wachtwoord moet minimaal 7 karakters!"
 
