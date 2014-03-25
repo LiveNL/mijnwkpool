17ddf44e45abe67b555beb6fafca33e4949abf18
Peter Alserda <peteralserda@hotmail.com>
2014-03-25 14:50:24 +0100

    error message update

--- app/helpers/pool_helper.rb
+++ app/helpers/pool_helper.rb
@@ -1,8 +1,18 @@
 module PoolHelper
-	def error_message_for(field, options = {:prepend_text => ""})
-		error_message = @pool.errors[field][0]
-		if error_message
-			raw "#{options[:prepend_text]}"
+
+	def error_message_password1(field)
+		error_messagepassword1 = @pool.errors[field]
+		if error_messagepassword1
+			raw "#{error_messagepassword1[0]}
+			<br/>#{error_messagepassword1[1]}"
 		end
 	end
+
+	def error_message_password2(field)
+		error_messagepassword2 = @pool.errors[field]
+		if error_messagepassword2
+			raw "#{error_messagepassword2[0]}"
+		end
+	end
+
 end
