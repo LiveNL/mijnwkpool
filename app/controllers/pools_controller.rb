f15b30c152759fb6d73391fe526fdd6ce522d192
Peter Alserda <peteralserda@hotmail.com>
2014-03-31 14:59:20 +0200

    bcrypt validations problem added

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -4,7 +4,7 @@ class PoolsController < ApplicationController
   end
 
   def create
-    @pool = Pool.new(:password) 
+    @pool = Pool.new(pool_params)
     if @pool.save
       @pool.poolmemberships.create(:user => current_user, :role => 1)
       redirect_to :controller => 'pools', :action => "index"
--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,15 +1,22 @@
 class Pool < ActiveRecord::Base
+	include BCrypt
 	has_many :poolmemberships
 	has_many :users, :through => :poolmemberships
 	before_save :encrypt_password
 
-	validates_length_of :name, :in => 3..35, message: "* De pool naam moet tussen 3-35 karakters hebben."
-	validates :name, uniqueness: { message: "* Deze pool naam is al bezet." }
-	validates_format_of :name, with: /\w+([- ]{,1})/i
+
+
+	# validates_length_of :name, :in => 3..35, message: "* De pool naam moet tussen 3-35 karakters hebben."
+	# validates :name, uniqueness: { message: "* Deze pool naam is al bezet." }
+	# validates_format_of :name, with: /\w+([- ]{,1})/i
 
 	validates_confirmation_of :password, if: :private_pool?, message: "* De wachtwoorden komen niet overeen!"
- 	validates_length_of :password, :in => 5..50, if: :private_pool?, message: "* Het wachtwoord moet minimaal 5 karakters!"
- 	validates_length_of :password_confirmation, :in => 5..50, if: :private_pool?, message: "* De wachtwoorden komen niet overeen!"
+ 	validates_length_of :password, :in => 5..35, if: :private_pool?
+ 	validates_length_of :password_confirmation, :in => 5..35, if: :private_pool?, message: "* Het wachtwoord moet minimaal 5 karakters!"
+
+ 	def public_pool?
+ 		public == true
+ 	end
 
 	def private_pool?
 		public == false
@@ -21,5 +28,17 @@ class Pool < ActiveRecord::Base
 	      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
 	    end
   	end
+
+
+
+
+    def password
+      @password ||= Password.new(password_hash)
+    end
+
+    def password=(new_password)
+      @password = Password.create(new_password)
+      self.password_hash = @password
+    end
 
 end
