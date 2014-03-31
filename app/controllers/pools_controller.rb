f09d084d9237c3dcb6e7407e748d2bc3e70b0bdf
Sander Tuin <github@sandert.me>
2014-03-31 23:43:44 +0200

    ModelWizard added to PoolsController

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -4,17 +4,20 @@ class PoolsController < ApplicationController
   end
 
   def create
-    @pool = Pool.new(pool_params)
-    if @pool.save
-      @pool.poolmemberships.create(:user => current_user, :role => 1)
-      redirect_to :controller => 'pools', :action => "index"
-    else 
-    render "new"
-    end 
+    wizard = ModelWizard.new(Pool, session, params).process
+    @pool = wizard.object
+    if wizard.save
+      @pool.poolmemberships.create(user: current_user, role: 1)
+      redirect_to controller: 'pools', action: 'index'
+    else
+      render :new
+    end
   end
 
   private
-    def pool_params
-      params.require(:pool).permit(:name, :image, :public, :password, :password_confirmation)
-    end
+
+  def pool_params
+    params.require(:pool).permit(:name, :image, :public, :password,
+                                 :password_confirmation)
+  end
 end
