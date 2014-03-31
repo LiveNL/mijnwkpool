b5f89a7f2b5c817b381a6dcb2d5d4da4c6cec099
Sander Tuin <github@sandert.me>
2014-04-01 00:14:43 +0200

    PoolModel cleaned

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -6,6 +6,15 @@ class Pool < ActiveRecord::Base
   before_save :clear_passwords, if: :public_pool?
   before_save :password_validation, :encrypt_password, if: :private_pool?
 
+  # Name
+  validates_length_of :name, in: 3..35, message: '* De pool naam moet tussen 3-35 karakters hebben.'
+  validates :name, uniqueness: { message: '* Deze pool naam is al bezet.' }
+  validates_format_of :name, with: /\A[a-zA-Z0-9]+[ _-]{,1}\z/, message: 'Het is niet toegestaan meerdere \'-_ \' achter elkaar te plaatsen.'
+
+  # Password
+  validates_length_of :password_confirmation, in: 5..35, if: :private_pool?, message: '* Het wachtwoord moet minimaal 5 karakters!'
+  validates_confirmation_of :password, if: :private_pool?, message: '* De wachtwoorden komen niet overeen!'
+
   def public_pool?
     public == true
   end
@@ -13,12 +22,6 @@ class Pool < ActiveRecord::Base
   def private_pool?
     public == false
   end
-
-  # validates_length_of :name, :in => 3..35, message: "* De pool naam moet tussen 3-35 karakters hebben."
-  validates :name, uniqueness: { message: '* Deze pool naam is al bezet.' }
-  validates_format_of :name, with: /\A[a-zA-Z0-9]+[ _-]{,1}\z/, message: 'Het is niet toegestaan meerdere \'-_ \' achter elkaar te plaatsen.'
-  validates_length_of :password_confirmation, in: 5..35, if: :private_pool?, message: '* Het wachtwoord moet minimaal 5 karakters!'
-  validates_confirmation_of :password, if: :private_pool?, message: '* De wachtwoorden komen niet overeen!'
 
   def password_validation
     validates_length_of :password, in: 5..35, if: :private_pool?
