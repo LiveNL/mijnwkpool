027ce7edc73ad2f1e89916c5cfbc70ebca44ba7b
Sander Tuin <github@sandert.me>
2014-03-31 23:04:51 +0200

    PoolModel spec

---
+++ spec/models/pool_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Pool do
+
+  let(:pool) { Pool.new(name: 'The Next Web Leeuwarden') }
+
+  it 'is invalid without password if private' do
+    pool.public = false
+    pool.password = ''
+    pool.password_confirmation = ''
+
+    expect(pool).not_to be_valid
+
+    pool.password = 'ikbenwacht'
+    pool.password_confirmation = 'ikbenwacht'
+
+    expect(pool).to be_valid
+
+  end
+
+  it 'clears password if public on save' do
+    pool.public = true
+    pool.password = 'ikbenwacht'
+    pool.password_confirmation = 'ikbenwacht'
+
+    pool.save
+    expect(pool.password).to be_empty
+
+    pool.password = 'ikbenechtwelwachtwoord'
+
+    pool.save
+    expect(pool.password).to be_empty
+
+  end
+
+  it 'matches passwords if private' do
+    pool.public = false
+    pool.password = 'ikbenwacht'
+    pool.password_confirmation = 'ikbenwacht'
+
+    expect(pool).to be_valid
+
+    pool.password = 'ikbenwacht2'
+
+    expect(pool).not_to be_valid
+
+  end
+end
