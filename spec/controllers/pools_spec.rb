93e65632e54bbd80cfe601b9c342aa3481f9dc95
Sander Tuin <github@sandert.me>
2014-03-31 23:03:25 +0200

    PoolsController spec is invalid

--- spec/controllers/pools_spec.rb
+++ spec/controllers/pools_spec.rb
@@ -1,20 +1,19 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe PoolsController do
-	describe "Post create" do
-	  it "must have a current_user to create a pool" do
-	  	pool = Pool.create
-	  	post 'create', :pool => { :name => 'current_user'}
-	  end
-	end
+  describe 'Post create' do
+    it 'must have a current_user to create a pool' do
+      # pool = Pool.create
+      # post 'create', pool: { name: 'current_user' }
+    end
+  end
 end
 
-
-# describe MenuItemsController, "creating a new menu item" do
+# describe MenuItemsController, 'creating a new menu item' do
 #   integrate_views
 #   fixtures :menu_items
-  
-#   it "should redirect to index with a notice on successful save" do
+
+#   it 'should redirect to index with a notice on successful save' do
 #     MenuItem.any_instance.stubs(:valid?).returns(true)
 #     post 'create'
 #     assigns[:menu_item].should_not be_new_record
@@ -22,15 +21,15 @@ end
 #     response.should redirect_to(menu_items_path)
 #   end
 
-#   it "should re-render new template on failed save" do
+#   it 'should re-render new template on failed save' do
 #     MenuItem.any_instance.stubs(:valid?).returns(false)
 #     post 'create'
 #     assigns[:menu_item].should be_new_record
 #     flash[:notice].should be_nil
 #     response.should render_template('new')
 #   end
-  
-#   it "should pass params to menu item" do
+
+#   it 'should pass params to menu item' do
 #     post 'create', :menu_item => { :name => 'Plain' }
 #     assigns[:menu_item].name.should == 'Plain'
 #   end
