65561ef000d04333e15e8b100ac996c6b056db24
Sander Tuin <github@sandert.me>
2014-04-01 00:11:15 +0200

    Name Validation regex

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -15,8 +15,8 @@ class Pool < ActiveRecord::Base
   end
 
   # validates_length_of :name, :in => 3..35, message: "* De pool naam moet tussen 3-35 karakters hebben."
-  # validates :name, uniqueness: { message: "* Deze pool naam is al bezet." }
-  # validates_format_of :name, with: /\w+([- ]{,1})/i
+  validates :name, uniqueness: { message: '* Deze pool naam is al bezet.' }
+  validates_format_of :name, with: /\A[a-zA-Z0-9]+[ _-]{,1}\z/, message: 'Het is niet toegestaan meerdere \'-_ \' achter elkaar te plaatsen.'
   validates_length_of :password_confirmation, in: 5..35, if: :private_pool?, message: '* Het wachtwoord moet minimaal 5 karakters!'
   validates_confirmation_of :password, if: :private_pool?, message: '* De wachtwoorden komen niet overeen!'
 
