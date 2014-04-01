6810135fd67f7540951105f0147121586c48d621
Sander Tuin <github@sandert.me>
2014-04-01 12:11:03 +0200

    Image edit button and label for privacy confirmation

--- app/assets/javascripts/pool.js
+++ app/assets/javascripts/pool.js
@@ -2,34 +2,34 @@ $(function() {
   $input = $("input:radio[name='pool[public]']");
   $.each($input,function(k,v) {
     togglePasswordFields(v);
-  })
+  });
 
   $input.on('change', function() {
-    togglePasswordFields(this)
-  })
+    togglePasswordFields(this);
+  });
 
   $('a.togglepassword').on('click', function(e) {
-    $('a.togglepassword.hide').removeClass('hide')
-    $(this).addClass('hide')
+    $('a.togglepassword.hide').removeClass('hide');
+    $(this).addClass('hide');
 
     e.preventDefault();
     $('.password').each(function(){
       $this = $(this);
       if($this.hasClass('hide')) {
-        $this.removeClass('hide')
+        $this.removeClass('hide');
       } else {
-        $this.addClass('hide')
+        $this.addClass('hide');
       }
     });
-  })
+  });
 });
 
 function togglePasswordFields(e) {
   if(e.checked) {
     if (e.value == 'true') {
-      $("#private_pool_enabled").hide()
+      $("#private_pool_enabled").hide();
     } else {
-      $("#private_pool_enabled").show()
+      $("#private_pool_enabled").show();
     }
   }
 }
