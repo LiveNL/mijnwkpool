8c7a989b72f1f47677040e841151087b2fe92093
Peter Alserda <peteralserda@hotmail.com>
2014-04-22 20:01:08 +0200

    login debug, session + poolmemberships controller fixes added

--- app/controllers/poolmemberships_controller.rb
+++ app/controllers/poolmemberships_controller.rb
@@ -8,18 +8,16 @@ class PoolmembershipsController < ApplicationController
     pool = Pool.find(pool_id)
 
     if password && pool.authenticate(password) && !pool.is_public?
-      poolmembership = Poolmembership.new(user: second_user, pool_id: pool_id, role: 0)
+      poolmembership = Poolmembership.new(user: current_user, pool_id: pool_id, role: 0)
       poolmembership.save
-      redirect_to '/pools'
+      redirect_to pools_path
+    elsif pool.is_public?
+      poolmembership = Poolmembership.new(user: current_user, pool_id: pool_id, role: 0)
+      poolmembership.save
+      redirect_to pools_path
     else
       flash[:error] = 'Je hebt geen toestemming om je bij deze pool aan te sluiten'
       redirect_to :back
-    end
-
-    if pool.is_public?
-      poolmembership = Poolmembership.new(user: second_user, pool_id: pool_id, role: 0)
-      poolmembership.save
-      redirect_to '/pools'
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,46 @@ class User < ActiveRecord::Base
 	
   	has_many :poolmemberships
   	has_many :pools, through: :poolmemberships
+
+    validates :name,
+            length: {
+              minimum: 2,
+              maximum: 35,
+              message: '* Je naam moet tussen de 2 en 35 karakters lang zijn.'
+            },
+            uniqueness: {
+              message: '* Deze naam is al bezet.'
+            }
+
+    validates :nickname,
+            length: {
+              minimum: 2,
+              maximum: 35,
+              message: '* Je nickname moet tussen de 2 en 35 karakters lang zijn.'
+            },
+            uniqueness: {
+              message: '* Deze nicknaam is al bezet.'
+            }
+
+    validates :email,
+            uniqueness: {
+              message: '* Dit e-mail adres is al in gebruik.'
+            },
+            format: {
+              with: /[a-zA-Z0-9._%-]+@(?:[a-zA-Z0-9-]+\.)+(com|net|org|info|biz|me|edu|gov|nl)/,
+              message: 'Voer een geldig e-mail adres in!'
+            }
+
+
+    validates :password,
+            
+            presence: false,
+            allow_blank: false,
+            length: {
+              minimum: 5,
+              message: '* Het wachtwoord moet minimaal 5 karakters!'
+            },
+            confirmation: {
+              message: '* De wachtwoorden komen niet overeen!'
+            }    
 end
