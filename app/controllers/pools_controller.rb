e736d3e830b37c88c08c9f267abdf4b357d7f802
Peter Alserda <peteralserda@hotmail.com>
2014-04-23 11:42:45 +0200

    creating pools restriction with max poolmemberships added

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -9,11 +9,16 @@ class PoolsController < ApplicationController
 
   def create
     @pool = Pool.new(pool_params)
-    if @pool.save
-      @pool.poolmemberships.create(user: current_user, role: 1)
-      render 'confirmation'
+    if current_user.poolmemberships.count == 3
+      flash[:error] = 'Je zit al in 3 pools.'
+      redirect_to pools_path
     else
-      render 'new'
+      if @pool.save
+        @pool.poolmemberships.create(user: current_user, role: 1)
+        render 'confirmation'
+      else
+        render 'new'
+      end
     end
   end
 
