9b2fdf709a0a895b80bf6a486f503da22b9814d0
Sander Tuin <github@sandert.me>
2014-04-24 16:36:47 +0200

    Navigation

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -1,1 +1,8 @@
 document.addEventListener("touchstart", function(){}, true);
+
+function menuButton(){
+  $('#nav-open-btn, #nav-close-btn').on('click', function(e){
+    e.preventDefault();
+    $('#mobile-nav').toggleClass('closed');
+  });
+}
