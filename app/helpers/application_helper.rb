97ed8213804db601ca6a0f184d804df5cb025301
Peter Alserda <peteralserda@hotmail.com>
2014-04-26 20:47:47 +0200

    active nav added (without styling)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, page_title.to_s
   end
+  
+  def is_active?(link_path)
+    if current_page?(link_path)
+      "class=active"
+    else
+      ""
+    end
+  end
 end
