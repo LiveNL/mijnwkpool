2b6852f80565ed5ac5989676795ecc773fc55786
Sander Tuin <github@sandert.me>
2014-05-08 14:40:45 +0200

    Deploy to from dev2 -> dev

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'Mijn_Wk_Pool'
 set :repo_url, 'git@github.com:LiveNL/mijnwkpool.git'
 
 set :branch, 'beta1'
-set :deploy_to, '/var/www/mijnwkpool.com/dev2'
+set :deploy_to, '/var/www/mijnwkpool.com/dev'
 set :scm, :git
 set :use_sudo, false
 set :format, :pretty
