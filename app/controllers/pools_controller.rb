87271423578653894d1b50695c6f0e60b53319a8
Sander Tuin <github@sandert.me>
2014-05-12 15:02:20 +0200

    Pools allowed to 99,Nav hovers,Invite on mobile,pools#new error for max pools

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,5 +1,5 @@
 class PoolsController < ApplicationController
-  require 'will_paginate/array' 
+  require 'will_paginate/array'
   skip_before_filter :ensure_user, :only => [:show]
 
   def index
@@ -11,22 +11,22 @@ class PoolsController < ApplicationController
             @pools = Pool.where("maximum_membership >= ? AND maximum_membership <= ?", params[:playerssearch1], params[:playerssearch2])
           end
         when '1'
-          @pools = Pool.find_by_sql ["select * from pools where 
+          @pools = Pool.find_by_sql ["select * from pools where
           (maximum_membership >= ? AND maximum_membership <= ?) AND (is_public == ?)", params[:playerssearch1], params[:playerssearch2], true]
         when '2'
-          @pools = Pool.find_by_sql ["select * from pools where 
+          @pools = Pool.find_by_sql ["select * from pools where
           (maximum_membership >= ? AND maximum_membership <= ?) AND (is_public == ?)", params[:playerssearch1], params[:playerssearch2], false]
         else
           @pools = Pool.all
       end
       poolspace = params[:poolspace] # "value1"
-      case poolspace 
+      case poolspace
         when '1'
         when '2'
-          @pools = Pool.find_by_sql ["select * from pools p where 
-            (maximum_membership >= ? AND maximum_membership <= ?) AND 
-            p.maximum_membership > (select count(*) from poolmemberships pm where pm.pool_id = p.id )", 
-            params[:playerssearch1], params[:playerssearch2]]  
+          @pools = Pool.find_by_sql ["select * from pools p where
+            (maximum_membership >= ? AND maximum_membership <= ?) AND
+            p.maximum_membership > (select count(*) from poolmemberships pm where pm.pool_id = p.id )",
+            params[:playerssearch1], params[:playerssearch2]]
         else
       end
       if !params[:search].nil?
@@ -35,14 +35,19 @@ class PoolsController < ApplicationController
   end
 
   def new
-    @pool = Pool.new
+    if current_user.poolmemberships.count == 99
+      flash[:error] = 'Je zit al in 3 pools.'
+      redirect_to :back
+    else
+      @pool = Pool.new
+    end
   end
 
   def create
     @pool = Pool.new(pool_params)
-    if current_user.poolmemberships.count == 3
+    if current_user.poolmemberships.count == 99
       flash[:error] = 'Je zit al in 3 pools.'
-      redirect_to pools_path
+      redirect_to :back
     else
       if @pool.save
         @pool.poolmemberships.create(user: current_user, role: 1)
