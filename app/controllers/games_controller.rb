ed84852163df6149bce80abb2e7e4685ec41ae2a
Peter Alserda <peteralserda@hotmail.com>
2014-05-14 10:37:27 +0200

    new games added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,25 @@ class GamesController < ApplicationController
     @games = Game.all
     @teams = Team.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      flash[:success] = 'Game toegevoegd.'
+      redirect_to games_path
+    else
+      render 'new'
+    end
+  end
+
+
+private
+
+  def game_params
+    params.require(:game).permit(:score1, :score2, :team1_id, :team2_id, :type, :date, :time)
+  end
 end
