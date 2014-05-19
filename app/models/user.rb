b04a4ee02615b20fd84705531965c45afa10865d
Sander Tuin <github@sandert.me>
2014-05-19 14:17:05 +0200

    Removed unused method is_subscribed

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,10 +45,6 @@ class User < ActiveRecord::Base
             },
             if: :password
 
-  def is_subscribed?
-    self.subscribed
-  end
-
   def send_password_reset
     generate_token(:password_reset_token)
     self.password_reset_sent_at = Time.zone.now
