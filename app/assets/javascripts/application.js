dd6267d22b914e63bcf405cec63586ea236fba8d
Peter Alserda <peteralserda@hotmail.com>
2014-05-22 15:43:50 +0200

    new prediction in table fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require best_in_place
 //= require best_in_place.purr
+//= require turbolinks
 //= require_tree ./vendor
 //= require_tree .
