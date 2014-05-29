a6783c426de851b893d45301822f6324b8a705a9
Sander Tuin <github@sandert.me>
2014-05-29 15:56:24 +0200

    Changed deploy to test from main

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'Mijn_Wk_Pool'
 set :repo_url, 'git@github.com:LiveNL/mijnwkpool.git'
 
 set :branch, 'bugfixes'
-set :deploy_to, '/var/www/mijnwkpool.com/main'
+set :deploy_to, '/var/www/mijnwkpool.com/test'
 set :scm, :git
 set :use_sudo, false
 set :format, :pretty
