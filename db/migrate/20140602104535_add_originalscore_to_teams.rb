ef9176062e39443b764c459374b79e5aeb7b55ee
Peter Alserda <peteralserda@hotmail.com>
2014-06-02 12:48:36 +0200

    migrations fixed 2

--- db/migrate/20140602104535_add_originalscore_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddOriginalscoreToTeams < ActiveRecord::Migration
-  def change
-    add_column :teams, :originalscore, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602104535) do
+ActiveRecord::Schema.define(version: 20140602104812) do
 
   create_table "games", force: true do |t|
     t.datetime "created_at"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20140602104535) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.integer  "originalscore"
+    t.integer  "originalposition"
   end
 
   create_table "users", force: true do |t|
